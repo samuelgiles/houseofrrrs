4d37d6a6d6c381749bd42c4f3c40094b657da16f
Samuel Giles <livelikeasam@googlemail.com>
2014-01-07 00:05:07 +0000

    Added barclays Ping It URI to Payment
    Decode the QR Code that the Barclays site can generate, technically being that QR Code readers just open the URI the same URI in just a link should work

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,17 @@ class Payment < ActiveRecord::Base
   belongs_to :user
   belongs_to :destination_user, :class_name => "User"
   has_many :shares, :class_name => "Share", :foreign_key => "paid"
+
+  #create a Barclays Ping It URI for the payment
+  def barclaysPingItURI
+
+  	#barclays://08/07543651833/3.50/07543651833//
+  	mode = "08"
+  	destinationNumber = self.user.phone
+  	setPrice = self.amount
+  	confirmNumber = self.destination_user.phone
+  	uri = "barclays://#{mode}/#{destinationNumber}/#{setPrice}/#{confirmNumber}"
+
+  end
+
 end
